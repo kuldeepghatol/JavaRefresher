2ac264d8d4959d622baf548d947411f381007ed2
Kuldeep <kuldeepghatol@gmail.com>
2022-12-12 22:22:05 +0530

    code for printing the waveform from 2D array row wise

--- Static2DArray.java
+++ Static2DArray.java
@@ -4,9 +4,36 @@ public class Static2DArray {
 
     public static void main(String[] args) {
         //simpleArrayInputAndPrint();
-        printWaveFormColumnWise();
+       // printWaveFormColumnWise();
+        printWaveFormRowWise();
     }
 
+
+    public static void printWaveFormRowWise() {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int [][] mat = new int[n][n];
+
+        for(int i =0; i<n; i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = s.nextInt();
+            }
+        }
+
+        for(int i =0; i<n; i++){
+            if(i%2 == 0){
+                for(int j=0;j<n;j++){
+                    System.out.print(mat[i][j]+" ");
+                }
+            }else{
+                for(int j=n-1;j>=0;j--){
+                    System.out.print(mat[i][j]+" ");
+                }
+            }
+        }
+    }
     public static void printWaveFormColumnWise() {
         Scanner s = new Scanner(System.in);
 
